b82dac7849810f452933a3392d14b2315a3dda32
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 21:18:12 +0300

    RXJS-withLatestFrom

--- index.ts
+++ index.ts
@@ -1,22 +1,18 @@
 import { withLatestFrom, map } from 'rxjs/operators';
 import { interval } from 'rxjs';
 
-
 // Example 1: Latest value from quicker second source
+
 //emit every 5s
-const source = interval(5000);
+const srcInterval = interval(5000);
 //emit every 1s
-const secondSource = interval(1000);
-const example = source.pipe(
-  withLatestFrom(secondSource),
+const secondSrc = interval(1000);
+
+const example = srcInterval.pipe(
+  withLatestFrom(secondSrc),
   map(([first, second]) => {
     return `First Source (5s): ${first} Second Source (1s): ${second}`;
   })
 );
-/*
-  "First Source (5s): 0 Second Source (1s): 4"
-  "First Source (5s): 1 Second Source (1s): 9"
-  "First Source (5s): 2 Second Source (1s): 14"
-  ...
-*/
+
 const subscribe = example.subscribe((val) => console.log(val));
